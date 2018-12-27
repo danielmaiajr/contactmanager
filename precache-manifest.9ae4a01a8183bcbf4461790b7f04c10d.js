5d16fc6859da2338974e396ce8fb8e741c055d8f
Daniel Maia <daniel.maia.jr12@gmail.com>
2018-12-27 17:39:31 -0200

    Updates

---
+++ precache-manifest.9ae4a01a8183bcbf4461790b7f04c10d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5acded6385afda19fc16",
+    "url": "/contactmanager/static/js/main.5acded63.chunk.js"
+  },
+  {
+    "revision": "26f29a43da0145c4ff10",
+    "url": "/contactmanager/static/css/1.66873fbb.chunk.css"
+  },
+  {
+    "revision": "26f29a43da0145c4ff10",
+    "url": "/contactmanager/static/js/1.26f29a43.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "ea8a1342d39f4ddcd73681fc2c51d330",
+    "url": "/contactmanager/index.html"
+  }
+];
